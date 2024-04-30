1da77297bb1d8bed22ea1dbbda83fbf1b8b51536
daniel-locatelli <daniel.nlocatelli@gmail.com>
2024-04-30 17:47:50 +0200

    Included many dependencies/integrations: @astrojs/tailwind, astro-icon, @pagefind/default-ui

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,14 @@
 import { defineConfig } from "astro/config";
 import { CUSTOM_DOMAIN, BASE_PATH } from "./src/server-constants";
-import IndexPostImageDownloader from "./src/integrations/index-posts-cover-image-downloader";
-import PostsFilesDownloader from "./src/integrations/posts-files-downloader";
+import AllFilesDownloader from "./src/integrations/all-files-downloader";
 import CustomIconDownloader from "./src/integrations/custom-icon-downloader";
 import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
 
 const getSite = function () {
   if (CUSTOM_DOMAIN) {
     return new URL(BASE_PATH, `https://${CUSTOM_DOMAIN}`).toString();
   }
-
   if (process.env.CF_PAGES && CF_PAGES_BRANCH !== "main") {
     if (process.env.CF_PAGES_BRANCH !== "main") {
       return new URL(BASE_PATH, process.env.CF_PAGES_URL).toString();
@@ -19,25 +18,17 @@ const getSite = function () {
     // It is when the site is only on CF pages
     return new URL(
       BASE_PATH,
-      `https://${new URL(process.env.CF_PAGES_URL).host
-        .split(".")
-        .slice(1)
-        .join(".")}`
+      `https://${new URL(process.env.CF_PAGES_URL).host.split(".").slice(1).join(".")}`
     ).toString();
   }
-
   return new URL(BASE_PATH, "http://localhost:4321").toString();
 };
 
+// https://astro.build/config
 export default defineConfig({
   site: getSite(),
   base: BASE_PATH,
-  integrations: [
-    sitemap(),
-    // IndexPostImageDownloader(),
-    // PostsFilesDownloader(),
-    // PeoplePhotoDownloader(),
-    // CustomIconDownloader(),
-  ],
+  integrations: [sitemap(), AllFilesDownloader(), tailwind()],
+  // CustomIconDownloader(),
   prefetch: true,
 });
