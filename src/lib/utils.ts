e2965e3afc745a271e182d87e27f6c1ae485b72e
daniel-locatelli <daniel.nlocatelli@gmail.com>
2024-06-01 17:30:32 +0200

    Removed prism, I am using astro native Chiki for Code highlight

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,3 +1,7 @@
+import type { BundledLanguage } from "../../node_modules/shiki/dist/langs.d.mts";
+
+type SpecialLanguage = "text" | "txt" | "ansi";
+
 export const pathJoin = (path: string, subPath: string) => {
   return (
     "/" +
@@ -33,4 +37,36 @@ export function titleToSlug(string: string) {
     .replace(/[^\w\s-]/g, "")
     .replace(/[-\s]+/g, "-");
   return simplifiedString;
+}
+
+export function notionLangToShiki(
+  lang: any
+): BundledLanguage | SpecialLanguage {
+  switch (lang) {
+    case "arduino":
+      return "cpp";
+    case "flow":
+      return "javascript";
+    case "fortran":
+      return "fortran-fixed-form";
+    case "livescript":
+      return "javascript";
+    case "markup":
+      return "markdown";
+    case "plain text":
+      return "text";
+    case "protobuf":
+      return "proto";
+    case "reason":
+      return "javascript";
+    case "vb.net":
+      return "vb";
+    case "visual basic":
+      return "vb";
+    case "webassembly":
+      return "wasm";
+    case "java/c/c++/c#":
+      return "c";
+  }
+  return lang;
 }
