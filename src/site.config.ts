94770209582987cb4be95ddf9c129378b15a767d
Daniel Locatelli <daniel.nlocatelli@gmail.com>
2025-05-25 14:49:37 +0200

    changed email

--- src/site.config.ts
+++ src/site.config.ts
@@ -8,7 +8,7 @@ export const siteConfig: SiteConfig = {
   // Meta property used as the default description meta property
   description: "An opinionated starter theme for Astro",
   // Date.prototype.toLocaleDateString() parameters, found in src/utils/date.ts.
-  email: "architect.locatelli@gmail.com",
+  email: "contact@daniellocatelli.com",
   github: "https://github.com/daniel-locatelli",
   linkedin: "https://www.linkedin.com/in/daniel-locatelli",
   whatsapp: "https://wa.me/message/MFRZO3U7RABUM1",
