3d26817a7f54b1abb89747003a1de671d94ed968
Daniel Locatelli <daniel.nlocatelli@gmail.com>
2026-01-19 16:08:31 +0100

    adding full name to the page author

--- src/site.config.ts
+++ src/site.config.ts
@@ -2,7 +2,7 @@ import type { SiteConfig } from "@/types";
 
 export const siteConfig: SiteConfig = {
   // Used as both a meta property (src/components/BaseHead.astro L:31 + L:49) & the generated satori png (src/pages/og-image/[slug].png.ts)
-  author: "Daniel Locatelli",
+  author: "Daniel Nunes Locatelli",
   // Meta property used to construct the meta title property, found in src/components/BaseHead.astro L:11
   title: "Daniel Locatelli",
   // Meta property used as the default description meta property
